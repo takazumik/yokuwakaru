13ca00ad77f993c468c2b3022eaebff9275543e3
takazumi <takazumikwkm@gmail.com>
2020-03-05 16:10:48 +0900

    SQLファイルを更新しました

--- mini_bbs.sql
+++ mini_bbs.sql
@@ -3,22 +3,32 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 05, 2020 at 06:42 AM
+-- Generation Time: Mar 05, 2020 at 07:09 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `mini_bbs`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `picture` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `members`
@@ -28,6 +38,21 @@ INSERT INTO `members` (`id`, `name`, `email`, `password`, `picture`, `created`, 
 (6, 'mizubitashi', '1234', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '20200304030123ama.jpg', '2020-03-04 12:01:25', '2020-03-04 03:01:25'),
 (7, 'ama', '2345', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '20200304031042am.jpg', '2020-03-04 12:10:47', '2020-03-04 03:16:06'),
 (8, '5678', '5678', '2abd55e001c524cb2cf6300a89ca6366848a77d5', '20200304101957am.jpg', '2020-03-04 19:21:38', '2020-03-04 10:21:38');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `member_id` int(11) NOT NULL,
+  `reply_message_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `posts`
@@ -48,6 +73,34 @@ INSERT INTO `posts` (`id`, `message`, `member_id`, `reply_message_id`, `created`
 (19, 'キーボ', 6, 0, '2020-03-04 18:51:44', '2020-03-04 09:51:44'),
 (20, 'クセがすごい…', 7, 0, '2020-03-04 18:52:46', '2020-03-04 09:52:46');
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `members`
+--
+ALTER TABLE `members`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
